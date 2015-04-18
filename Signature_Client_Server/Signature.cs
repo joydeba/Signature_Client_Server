fa1bf775271cb08acb1fbbaeecba7d06f740498f
joydeba <joydeba@yahoo.com>
2015-04-18 21:11:41 +0600

    test

---
+++ Signature_Client_Server/Signature.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Signature_Client_Server
+{
+    class Signature
+    {
+        RSACryptoServiceProvider globaLrsa;
+        string globalHashAlgo;
+
+        private CspParameters GetCryptoServiceProvider()
+        {
+            // Create the CspParameters object 
+            CspParameters csp = new CspParameters();
+            // Set the key container name that has the _rsa key pair
+            csp.KeyContainerName = "temp Keys";
+            //Set the CSP Provider Type PROV_RSA_FULL
+            csp.ProviderType = 1;
+            csp.Flags = CspProviderFlags.UseDefaultKeyContainer;
+            //Set the CSP Provider Name
+            csp.ProviderName = "Microsoft Enhanced Cryptographic Provider v1.0";
+            return csp;
+        }
+
+        public Signature(string hashAlgo)
+        {
+            globaLrsa = new RSACryptoServiceProvider(GetCryptoServiceProvider());
+            globalHashAlgo = hashAlgo;
+        }
+
+        public byte[] Sign(byte[] hashvalue)
+        {
+            //Computes the signature for the specified hash value by encrypting it with the private key
+            byte[] sign = globaLrsa.SignHash(hashvalue, CryptoConfig.MapNameToOID(globalHashAlgo)); //128 byte signature
+
+            return sign;
+        }
+
+        public bool Validate(string file, byte[] sign)
+        {
+            HashAFile haf = new HashAFile(file);
+            byte[] hash = haf.HashFile();
+            bool test = globaLrsa.VerifyHash(hash, CryptoConfig.MapNameToOID(globalHashAlgo), sign);
+            return test;
+        }
+
+
+    }
+}
